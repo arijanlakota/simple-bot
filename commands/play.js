918430110a5943dc14938858c2dc9b915f8a3256
Arijan <arijannp2005@gmail.com>
2021-08-13 19:03:20 +0200

    new message

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,10 @@
-var wordArr = ["povodac","varijabla","deklaracija","dijapazon","kontradiktornost","privilegija","potrcati","znatizelja","suton","developer","satirican","vodolija"]
+var wordArr = ["povodac","varijabla","deklaracija","dijapazon",
+"kontradiktornost","privilegija","potrcati","znatizelja","suton","developer","satirican","vodolija","impresionizam","instrumentarijum",
+"balada","transedentalno","vitamini","nagovestaj","proporcija","kantautor","pozamasan","natalitet","centrafor","lansirati"]
 var pokusaji;
 var randomWord;
 var pomocnaLista;
+var players = {}
 module.exports = {
 	name: 'play',
 	aliases: false,
@@ -12,37 +15,39 @@ module.exports = {
 	usage:false,
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
+        var userId = msg.author.id
         if(args[0] == "start"){
-            randomWord = wordArr[Math.floor(Math.random() * 9)].split("")
+            randomWord = wordArr[Math.floor(Math.random() * wordArr.length)].split("")
             pomocnaLista = []
             for(var i =0;i < randomWord.length;i++){
                 pomocnaLista.push(" . ")
             
             }
             pokusaji = 0;
+            players[userId] = {x:randomWord,y:pomocnaLista,z:pokusaji}
             return msg.reply("game sterted, we choose a random word ")
         }
-        if(randomWord && pokusaji <= 12 && pomocnaLista.join("") !== randomWord.join("")){
+        if(players[userId].x && players[userId].z <= 12 && players[userId].x.join("") !== players[userId].y.join){
             if(args[0].length > 1){
-                if(args[0] === randomWord.join("")){
+                if(args[0] === players[userId].x.join("")){
                     return msg.reply("You are right,the game is over")
                 }
-                pokusaji++
+                players[userId].z++
                 return msg.reply("you didn't guess right")
             }
-            if(randomWord.includes(args[0])){
-                for(i = 0;i<randomWord.length;i++){
-                if(randomWord[i] == args[0]){
+            if(players[userId].x.includes(args[0])){
+                for(i = 0;i<players[userId].x.length;i++){
+                if(players[userId].x[i] == args[0]){
                         pomocnaLista[i] = args[0]
                     }
                 }
-                return msg.reply(pomocnaLista.join(" "))
+                return msg.reply(players[userId].y.join(" "))
                 }
-                pokusaji++
+                players[userId].z++
                 return msg.reply("there is no such latter in our word")
             }
-        return msg.reply("game over the word was",randomWord.join(""))
-        randomWord = undefined
+        return msg.reply("game over the word was",players[userId].x.join(""))
+        players[userId] = undefined
 
 	}
 };
