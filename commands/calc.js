7edeea7be810931b1666a441dd48a9c1709a40b2
Arijan <arijannp2005@gmail.com>
2021-08-16 20:25:45 +0200

    some changes

--- commands/calc.js
+++ commands/calc.js
@@ -8,17 +8,20 @@ module.exports = {
 	usage:false,
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
-    var obim
-    var povrsina
-    var inputValue = parseInt(args[1])
-    if(args[0] == "krug") {
+    let obim
+    let povrsina
+    let keyWord = args[0]
+    let inputValue = parseInt(args[1])
+    let inputValue2 = parseInt(args[2])
+    let inputValue3 = parseInt(args[3])
+    if(keyWord == "krug") {
         povrsina = Math.pow(inputValue,2)*Math.PI
         obim = 2 * inputValue*Math.PI
         msg.reply(`Povrsina je: ${povrsina}`)
         msg.reply(`Obim je: ${obim}`)
         
     } 
-    else if(args[0] == "kvadrat"){
+    else if(keyWord == "kvadrat"){
         
         povrsina = Math.pow(inputValue,2)
         obim = 4 * inputValue
@@ -27,22 +30,24 @@ module.exports = {
         msg.reply(`Povrsina je: ${povrsina}`)
         msg.reply(`Obim je: ${obim}`)
     }  
-    else if(args[0] == "trougao"){
-        var a = inputValue
-        var b = parseInt(args[2])
-        var c = parseInt(args[3])
-        if(!(a + b > c && b + c > a && a + c > b)){
-            msg.reply("Nemoguce je formirati trougao od datih duzina")
+    else if(keyWord == "trougao"){
+        let a = inputValue
+        let b = inputValue2
+        let c = inputValue3
+        let isTriangle = a + b > c && b + c > a && a + c > b
+        if(!isTriangle){
+            return msg.reply("Nemoguce je formirati trougao od datih duzina")
         }
         obim = a + b + c
-        var s = obim / 2
-        povrsina = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+        let s = obim / 2
+        let povrsinaH = s * (s - a) * (s - b) * (s - c)
+        povrsina = Math.sqrt(povrsinaH)
         msg.reply(`Povrsina je: ${povrsina}`)
         msg.reply(`Obim je: ${obim}`)
     }
-    else if(args[0] == "pravougaonik"){
-        povrsina = parseInt(args[1]) * parseInt(args[2])
-        obim = 2 * (args[1] + args[2])
+    else if(keyWord == "pravougaonik"){
+        povrsina = inputValue * inputValue2
+        obim = 2 * (inputValue + inputValue2)
         msg.reply(`Povrsina je: ${povrsina}`)
         msg.reply(`Obim je: ${obim}`)
     }
